cd4bf8ae34d2b8cb2cc8a87ea69812cdc48e7836
Scott Thomas <smthom05@gmail.com>
2019-01-14 13:08:32 -0700

    added an about me page. Also made a test to check the user can get to the about me page

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,6 +2,27 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']]
+    case env["PATH_INFO"]
+    when '/' then index
+    when '/about' then about
+    else
+      error
+    end
+  end
+
+  def self.index
+    render_view('index.html')
+  end
+
+  def self.about
+    render_view('about.html')
+  end
+
+  def self.error
+    render_view('error.html', '404')
+  end
+
+  def self.render_view(page, code = '200')
+    [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
   end
 end
